cf7c9b952f83da4595d67414b1fd67a0051519f8
Viktoriia Svichkar <vitasvichkar@gmail.com>
2024-01-21 02:28:06 +0200

    feat: create a file and write data to it

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const { stdin, stdout } = process;
+const path = require('path');
+
+// const filePath = path.join(__dirname, '02-write-file.txt');
+let text = '';
+
+fs.writeFile(path.join(__dirname, '02-write-file.txt'), '', (err) => {
+  if (err) throw err;
+  // console.log('File created');
+  stdout.write('hello, write something\n');
+
+  stdin.on('data', (data) => {
+    text += data;
+
+    fs.writeFile('02-write-file/02-write-file.txt', text, (err) => {
+      if (err) throw err;
+    });
+  });
+});
+process.on('SIGINT', () => {
+  console.log('goodbye');
+  process.exit();
+});
